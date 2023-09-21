16fbd226c8c9fd51d552a00da592ef345472df3f
fa <zuchairul@gmail.com>
2023-09-21 19:50:18 +0800

    setup db and middleware

---
+++ db.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const db = mongoose.connect(process.env.DB_ACCESS).then(() => {
+  console.log('database connected');
+});
+
+module.exports = db;
