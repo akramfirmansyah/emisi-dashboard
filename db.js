2efcffaa1b148ed76d904b64343358f91f7e4d9a
akramfirmansyah <akram.firman@gmail.com>
2023-09-28 20:11:55 +0800

    fix config for deployment

--- db.js
+++ db.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const db = mongoose.connect(process.env.DB_ACCESS, {
-  dbName: process.env.DB_NAME
-}).then(() => {
-  console.log("database connected");
-});
+const connect = async () => {
+  try {
+    const db = await mongoose.connect(process.env.DB_ACCESS, {
+      dbName: process.env.DB_NAME,
+    });
 
-module.exports = db;
+    const { name, host } = db.connection;
+    console.log(`Database connected to ${name}, host ${host}`);
+  } catch (error) {
+    console.log("There is an error.", error);
+  }
+};
+module.exports = connect;
