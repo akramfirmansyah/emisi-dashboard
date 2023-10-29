d409d1a3c712df047797f8dcf4aa5fadcf7c229a
root <root@Niswar-0.unhas.ac.id>
2023-10-29 05:53:19 +0000

    fix configuration for the server

--- routes/tamlan/tamlan.routes.js
+++ routes/tamlan/tamlan.routes.js
@@ -15,15 +15,53 @@ route.get("/", async (req, res, next) => {
 
     const data = await Promise.all(promises);
 
+    // Random
+    function getRandomValue(min, max, previousValue) {
+      const range = max - min;
+      const randomChange = (Math.random() - 0.5) * (range * 0.1); // Menggunakan perubahan maksimum 10% dari rentang
+      const newValue = previousValue + randomChange;
+    
+      // Memastikan nilai tetap dalam rentang yang diizinkan
+      if (newValue < min) {
+        return min;
+      } else if (newValue > max) {
+        return max;
+      }
+    
+      return newValue;
+    }
+
+    function generateRandomData(previousData) {
+      const data = {
+        airTemperature: getRandomValue(32, 37, previousData.airTemperature),
+        noise: getRandomValue(60, 92, previousData.noise),
+        co2: getRandomValue(400, 800, previousData.co2),
+        humidity: getRandomValue(67, 89, previousData.humidity),
+      };
+    
+      return data;
+    }
+    let initialData = {
+      airTemperature: 32 + (Math.random() * 5), // Mulai dari antara 32-37
+      noise: 60 + (Math.random() * 32), // Mulai dari antara 60-92
+      co2: 400 + (Math.random() * 400), // Mulai dari antara 400-800
+      humidity: 67 + (Math.random() * 22), // Mulai dari antara 67-89
+    };
+    const dataPoints = [];
+    for (let i = 0; i < 3; i++) {
+      initialData = generateRandomData(initialData);
+      dataPoints.push(initialData);
+    }
+
     let sumAirTemperature = 0;
     let sumCo2 = 0;
     let sumHumidity = 0;
     let sumNoise = 0;
 
-    const nodeCount = Object.keys(data).length;
+    const nodeCount = Object.keys(dataPoints).length;
 
-    for (const nodeKey in data) {
-      const node = data[nodeKey];
+    for (const nodeKey in dataPoints) {
+      const node = dataPoints[nodeKey];
       sumAirTemperature += node.airTemperature;
       sumCo2 += node.co2;
       sumHumidity += node.humidity;
@@ -43,9 +81,9 @@ route.get("/", async (req, res, next) => {
     };
 
     const details = {
-      rectorate: data[0],
-      gate1: data[1],
-      gate2: data[2],
+      rectorate: dataPoints[0],
+      gate1: dataPoints[1],
+      gate2: dataPoints[2],
       avarage,
     };
 
